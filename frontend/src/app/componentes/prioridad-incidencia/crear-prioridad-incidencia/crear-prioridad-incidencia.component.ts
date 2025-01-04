814554971474f0b32d2f35f2b1b7581e9bdf1a53
Sebastián Plaza <seplarui@outlook.com>
2025-01-04 08:34:35 +0100

    GESTINC-32 - Crear Prioridad Incidencia

--- frontend/src/app/componentes/prioridad-incidencia/crear-prioridad-incidencia/crear-prioridad-incidencia.component.ts
+++ frontend/src/app/componentes/prioridad-incidencia/crear-prioridad-incidencia/crear-prioridad-incidencia.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuComponent } from '../../menu/menu.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../servicios/auth.service';
+import { PrioridadIncidencia } from '../../../modelos/prioridad-incidencia-interface';
 
 @Component({
   selector: 'app-crear-prioridad-incidencia',
@@ -11,6 +13,43 @@ import { RouterModule } from '@angular/router';
   templateUrl: './crear-prioridad-incidencia.component.html',
   styleUrl: './crear-prioridad-incidencia.component.css'
 })
-export class CrearPrioridadIncidenciaComponent {
+export class CrearPrioridadIncidenciaComponent implements OnInit {
 
+  errorMensaje: string = '';
+  prioridadIncidencia: PrioridadIncidencia = {
+    idPrioridad: 0,
+    nombre: '',
+    descripcion: ''
+  }
+  prioridadIncidenciaForm: FormGroup;
+
+  constructor(private authService: AuthService) {
+    this.prioridadIncidenciaForm = new FormGroup({
+      nombre: new FormControl('', Validators.required),
+      descripcion: new FormControl('', Validators.required)
+    })
+  }
+
+  ngOnInit(): void {
+    console.log('Componente cargado crear Prioridad Incidencia');
+  }
+
+  crearPrioridadIncidencia(): void {
+    if (this.prioridadIncidenciaForm.valid) {
+      this.authService.crearPrioridadIncidencia(this.prioridadIncidenciaForm.value).subscribe({
+        next: (response) => {
+          console.log('Prioridad Incidencia creada correctamente', response);
+          this.prioridadIncidenciaForm.reset();
+          this.errorMensaje = 'Prioridad Incidencia creada correctamente';
+          setTimeout(() => {
+            this.errorMensaje = '';
+          }, 3000);
+        },
+        error: (error) => {
+          console.error('Error al crear la prioridad incidencia', error);
+          this.errorMensaje = 'No se ha podido crear la prioridad incidencia';
+        }
+      });
+    }
+  }
 }
