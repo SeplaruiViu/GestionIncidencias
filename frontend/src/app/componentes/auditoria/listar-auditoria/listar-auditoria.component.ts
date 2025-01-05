7b7065f1e2b142685f7b4bc56666b9df3383bbfa
Sebastián Plaza <seplarui@outlook.com>
2025-01-05 14:06:32 +0100

    GESTINC-32 - Paginación Auditoria

--- frontend/src/app/componentes/auditoria/listar-auditoria/listar-auditoria.component.ts
+++ frontend/src/app/componentes/auditoria/listar-auditoria/listar-auditoria.component.ts
@@ -2,29 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { MenuComponent } from '../../menu/menu.component';
 import { AuthService } from '../../../servicios/auth.service';
 import { CommonModule } from '@angular/common';
+import { NgbPagination, NgbPaginationConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-listar-auditoria',
   standalone: true,
-  imports: [MenuComponent, CommonModule],
+  imports: [MenuComponent, CommonModule, NgbPagination],
   templateUrl: './listar-auditoria.component.html',
   styleUrl: './listar-auditoria.component.css'
 })
 export class ListarAuditoriaComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private paginationConfig: NgbPaginationConfig) { }
   auditorias: any[] = [];
   errorMensaje: string = '';
+  items: any[] = [];
+  pagedItems: any[] = [];
+  currentPage: number = 1;
+  pageSize: number = 10;
+
 
   ngOnInit(): void {
     this.listaAuditoria();
   }
 
+  onPageChange(page:number):void {
+    this.currentPage = page;
+    this.updatePagedItems();
+  }
+
+  private updatePagedItems(): void {
+    console.log(this.auditorias);
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedItems = this.auditorias.slice(startIndex, endIndex);
+  }
   listaAuditoria(): void {
     this.authService.getAuditoria().subscribe({
       next: (datos) => {
         this.auditorias = datos;
         console.log('Auditoria: ', datos);
+        this.updatePagedItems();
       },
       error: (error) => {
         console.error('Error al recuperar la auditoria: ', error);
